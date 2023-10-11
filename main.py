5025ccbedeb142c73cdba14838ece7b1bc750bf0
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2023-10-11 18:14:09 +0200

    feat: delete unneeded routes

--- main.py
+++ main.py
@@ -1,30 +1,21 @@
-from typing import Optional
-
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from starlette import status
+from starlette.responses import RedirectResponse
 
 import models
-from models.admins import Admins
 from models.database import engine
-from models.database import engine, db_dependency
-from models.supervisors import Supervisors
-from models.users import Users
-from populate import create_admins, create_supervisors
+from models.database import engine
 from routers import auth, users, refuges, reservation
 from routers import images
-from services.auth import (
-    get_user_id_from_token,
-    get_admin_id_from_token,
-    get_supervisor_id_from_token,
-)
-from services.user import (
-    get_user_from_id,
-    get_admin_from_id,
-    get_supervisor_from_id,
-)
 
-app = FastAPI()
+app = FastAPI(
+    title="RefuApp API",
+    description="User, refuge and reservations management API for RefuApp",
+    docs_url='/docs',
+    redoc_url='/redoc',
+    version="0.1.0",
+    swagger_ui_parameters={"syntaxHighlight.theme": "obsidian"},
+)
 app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(refuges.router)
@@ -34,33 +25,8 @@ app.include_router(reservation.router)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 models.database.Base.metadata.create_all(engine)
-create_admins()
-create_supervisors()
 
 
-@app.get("/user", status_code=status.HTTP_200_OK)
-async def user(user_id: get_user_id_from_token, db: db_dependency):
-    if user_id is None:
-        raise HTTPException(status_code=401, detail='Authentication failed')
-    current_user: Optional[Users] = get_user_from_id(user_id, db)
-    return {"User": current_user}
-
-
-@app.get("/admin", status_code=status.HTTP_200_OK)
-async def admin(admin_id: get_admin_id_from_token, db: db_dependency):
-    if admin_id is None:
-        raise HTTPException(status_code=401, detail='Authentication failed')
-    current_admin: Optional[Admins] = get_admin_from_id(admin_id, db)
-    return {"Admin": current_admin}
-
-
-@app.get("/supervisor", status_code=status.HTTP_200_OK)
-async def supervisor(
-    supervisor_id: get_supervisor_id_from_token, db: db_dependency
-):
-    if supervisor_id is None:
-        raise HTTPException(status_code=401, detail='Authentication failed')
-    current_supervisor: Optional[Supervisors] = get_supervisor_from_id(
-        supervisor_id, db
-    )
-    return {"Supervisor": current_supervisor}
+@app.get("/")
+def root():
+    return RedirectResponse(url='/docs')
