5b125d8ee4625fce358907e0895125a21e228327
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2023-10-19 16:27:09 +0200

    build(docker): sensible data as environment var

--- configuration/database.py
+++ configuration/database.py
@@ -6,7 +6,31 @@ from configuration.config import ConfigurationOption
 DATABASE_KEY = 'Database'
 
 
-class DatabaseUrl(ConfigurationOption):
+class DatabaseUser(ConfigurationOption):
     @staticmethod
     def get_value(configuration: ConfigParser) -> Optional[str]:
-        return configuration.get(DATABASE_KEY, 'Url')
+        return configuration.get(DATABASE_KEY, 'PostgresUser')
+
+
+class DatabasePassword(ConfigurationOption):
+    @staticmethod
+    def get_value(configuration: ConfigParser) -> Optional[str]:
+        return configuration.get(DATABASE_KEY, 'PostgresPassword')
+
+
+class DatabaseHost(ConfigurationOption):
+    @staticmethod
+    def get_value(configuration: ConfigParser) -> Optional[str]:
+        return configuration.get(DATABASE_KEY, 'PostgresHost')
+
+
+class DatabasePort(ConfigurationOption):
+    @staticmethod
+    def get_value(configuration: ConfigParser) -> Optional[str]:
+        return configuration.get(DATABASE_KEY, 'PostgresPort')
+
+
+class DatabaseName(ConfigurationOption):
+    @staticmethod
+    def get_value(configuration: ConfigParser) -> Optional[str]:
+        return configuration.get(DATABASE_KEY, 'PostgresDB')
--- models/database.py
+++ models/database.py
@@ -6,13 +6,25 @@ from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.ext.declarative import declarative_base
 
 from configuration.config import Configuration
-from configuration.database import DatabaseUrl
-
-SQLALCHEMY_DATABASE_URL = Configuration.get(DatabaseUrl)
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
+from configuration.database import (
+    DatabaseName,
+    DatabaseUser,
+    DatabasePassword,
+    DatabasePort,
+    DatabaseHost,
 )
+
+db_name = Configuration.get(DatabaseName)
+user = Configuration.get(DatabaseUser)
+password = Configuration.get(DatabasePassword)
+port = Configuration.get(DatabasePort)
+host = Configuration.get(DatabaseHost)
+
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db_name}"
+)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
